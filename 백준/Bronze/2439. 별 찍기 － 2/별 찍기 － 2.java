263af7bd98e94edcf10c4e6bda3d2cddb08cffde
Hand_min <152463277+sohnminjeong@users.noreply.github.com>
2024-05-31 18:58:36 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 360 ms, Memory: 22220 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        for(int i=1; i<=num; i++){
+            for(int j=1; j<=num-i; j++){
+                System.out.print(" ");
+            }
+            for(int k=1; k<=i; k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
