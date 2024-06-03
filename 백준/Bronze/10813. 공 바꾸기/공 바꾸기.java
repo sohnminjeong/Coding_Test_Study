35eb3334dc313e03c2168dae4117ab1bb16e0914
Hand_min <152463277+sohnminjeong@users.noreply.github.com>
2024-06-03 15:58:56 +0900

    [Bronze II] Title: 공 바꾸기, Time: 144 ms, Memory: 15916 KB -BaekjoonHub

---
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.java
@@ -0,0 +1,38 @@
+import java.util.*;
+import java.io.*;
+public class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int N = Integer.parseInt(st.nextToken());
+        int arr[] = new int[N];
+        int M = Integer.parseInt(st.nextToken());
+        int temp;
+        
+        for(int i=0; i<N; i++){
+            arr[i] = i+1;
+        }
+        
+        for(int j=0; j<M; j++){
+            st = new StringTokenizer(br.readLine());
+            
+            int I = Integer.parseInt(st.nextToken());
+            int J = Integer.parseInt(st.nextToken());
+            
+            temp = arr[I-1];
+            arr[I-1] = arr[J-1];
+            arr[J-1] = temp;
+        }
+        
+        for(int k=0; k<N; k++){
+            bw.write(arr[k] + " ");
+        }
+        br.close();
+        bw.flush();
+        bw.close();
+        
+    }
+}
