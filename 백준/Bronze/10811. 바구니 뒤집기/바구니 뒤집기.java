5f2b18c237b3c1332308fc5dfd6ca999963f8cb9
Hand_min <152463277+sohnminjeong@users.noreply.github.com>
2024-06-05 14:39:36 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 220 ms, Memory: 18512 KB -BaekjoonHub

---
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        
+        int[] arr = new int[N];
+        for(int i=0; i<N; i++){
+            arr[i] = i+1;
+        }
+        for(int i=0; i<M; i++){
+            int left = sc.nextInt()-1;
+            int right = sc.nextInt()-1;
+            while(left<right){
+                int temp = arr[left];
+                arr[left++] = arr[right];
+                arr[right--] = temp;
+            }
+        }
+        sc.close();
+        for(int ar : arr){
+            System.out.print(ar + " ");
+        }
+    }
+}
