2637702b562c64eec7ce324190b095610c31e1b6
Hand_min <152463277+sohnminjeong@users.noreply.github.com>
2024-06-07 15:21:47 +0900

    [Bronze V] Title: 아스키 코드, Time: 108 ms, Memory: 14104 KB -BaekjoonHub

--- 백준/Bronze/11654. 아스키 코드/아스키 코드.java
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -1,9 +1,10 @@
-import java.util.*;
+import java.io.*;
 public class Main{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        String a = sc.next();
-        int b = a.charAt(0);
-        System.out.println(b);
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        br.close();
+        int n = str.charAt(0);
+        System.out.println(n);
     }
 }
