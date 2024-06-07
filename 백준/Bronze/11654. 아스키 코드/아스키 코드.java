652b9f048d0cc29706d388a4c2435da6561e6187
Hand_min <152463277+sohnminjeong@users.noreply.github.com>
2024-06-07 15:16:57 +0900

    [Bronze V] Title: 아스키 코드, Time: 180 ms, Memory: 17624 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        int b = a.charAt(0);
+        System.out.println(b);
+    }
+}
