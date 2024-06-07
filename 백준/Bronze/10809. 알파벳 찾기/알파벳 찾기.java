ea0732ce020e59114e080c3be41b923c5a10abac
Hand_min <152463277+sohnminjeong@users.noreply.github.com>
2024-06-07 15:47:50 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 144 ms, Memory: 15876 KB -BaekjoonHub

---
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
@@ -0,0 +1,12 @@
+import java.io.*;
+public class Main{
+    public static void main(String[] args) throws IOException{
+       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        String str = br.readLine();
+        for(char c='a'; c<='z'; c++){
+            sb.append(str.indexOf(c)+" ");
+        }
+        System.out.println(sb);
+    }
+}
