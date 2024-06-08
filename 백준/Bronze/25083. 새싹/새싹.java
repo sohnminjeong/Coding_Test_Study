c66b5bb6d886860fcc6e976eaba87d931d7e9283
Hand_min <152463277+sohnminjeong@users.noreply.github.com>
2024-06-09 00:00:17 +0900

    [Bronze V] Title: 새싹, Time: 96 ms, Memory: 13876 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+       String a = "         ,r'\"7\n" + 	
+				   "r`-_   ,'  ,/\n" + 		
+				   " \\. \". L_r'\n" + 		
+				   "   `~\\/\n" + 			
+				   "      |\n" + 			
+				   "      |";
+ 
+		System.out.print(a);              
+    }
+}
