7320b905374103eabcb6703afa54607f85f68fcb
Hand_min <152463277+sohnminjeong@users.noreply.github.com>
2024-06-10 15:12:53 +0900

    [level 0] Title: 접미사인지 확인하기, Time: 0.02 ms, Memory: 73.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Solution {
+    public int solution(String my_string, String is_suffix) {
+        int answer = 0;
+        if(my_string.endsWith(is_suffix)){
+            answer=1;
+        }
+        return answer;
+    }
+}
