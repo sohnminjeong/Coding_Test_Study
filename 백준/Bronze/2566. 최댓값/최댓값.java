17428df39ce63f85680eb74d376693599c456daf
Hand_min <152463277+sohnminjeong@users.noreply.github.com>
2024-06-12 00:00:06 +0900

    [Bronze III] Title: 최댓값, Time: 248 ms, Memory: 18544 KB -BaekjoonHub

---
+++ 백준/Bronze/2566. 최댓값/최댓값.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int max=0;
+        int x=1;
+        int y=1;
+        int arr[][] = new int[9][9];
+        for(int i=0; i<9; i++){
+            for(int j=0; j<9; j++){
+                arr[i][j] = sc.nextInt();
+            }
+        }
+        
+        for(int i=0; i<9; i++){
+            for(int j=0; j<9; j++){
+                if(max<arr[i][j]){
+                    max = arr[i][j];
+                    x=i+1;
+                    y=j+1;
+                }
+            }
+        }
+        sc.close();
+        System.out.println(max);
+        System.out.println(x+" "+y);
+    }
+}
