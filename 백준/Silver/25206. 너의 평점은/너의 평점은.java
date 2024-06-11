b9be777c8174a10f939eac778750847a1f28e54d
Hand_min <152463277+sohnminjeong@users.noreply.github.com>
2024-06-11 23:22:12 +0900

    [Silver V] Title: 너의 평점은, Time: 144 ms, Memory: 14660 KB -BaekjoonHub

---
+++ 백준/Silver/25206. 너의 평점은/너의 평점은.java
@@ -0,0 +1,55 @@
+import java.util.*;
+import java.io.*;
+public class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        double sumTotalScore=0;
+        double sumNum=0;
+        
+        for(int i=0; i<20; i++){
+            st = new StringTokenizer(br.readLine());
+            String object = st.nextToken();
+            double num = Double.parseDouble(st.nextToken());
+            String alphabet = st.nextToken();
+            double score = 0;
+            
+            if(!(alphabet.equals("P"))){
+                switch(alphabet){
+                    case "A+":
+                        score=4.5;
+                        break;
+                     case "A0":
+                        score=4.0;
+                        break;
+                        case "B+":
+                        score=3.5;
+                        break;
+                        case "B0":
+                        score=3.0;
+                        break;
+                        case "C+":
+                        score=2.5;
+                        break;
+                        case "C0":
+                        score=2.0;
+                        break;
+                        case "D+":
+                        score=1.5;
+                        break;
+                        case "D0":
+                        score=1.0;
+                        break;
+                        case "F0":
+                        score=0.0;
+                        break;
+                    default:break;
+                }
+                sumTotalScore+=(num*score);
+                sumNum +=num;
+            }
+        }
+        System.out.printf("%.6f", sumTotalScore/sumNum);
+        br.close();
+    }
+}
