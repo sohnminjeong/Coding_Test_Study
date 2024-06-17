7cf3ab40e66d4973b4316059a6d98788a9f6913d
Hand_min <152463277+sohnminjeong@users.noreply.github.com>
2024-06-17 17:25:44 +0900

    [level 0] Title: A 강조하기, Time: 0.08 ms, Memory: 73.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.java
@@ -0,0 +1,8 @@
+import java.util.*;
+class Solution {
+    public String solution(String myString) {
+        myString = myString.toLowerCase();
+        myString = myString.replaceAll("a", "A");
+        return myString;
+    }
+}
