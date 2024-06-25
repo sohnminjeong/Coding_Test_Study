acc669ee43b604a59a013cc17df37793aa47bebb
Hand_min <152463277+sohnminjeong@users.noreply.github.com>
2024-06-25 15:27:06 +0900

    [level 0] Title: 커피 심부름, Time: 0.03 ms, Memory: 66.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181837. 커피 심부름/커피 심부름.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(String[] order) {
+        int answer = 0;
+        for(int i=0; i<order.length; i++){
+            if(order[i].contains("ame")||order[i].equals("anything")){
+                answer+=4500;
+            } else if(order[i].contains("latte")){
+                answer+=5000;
+            } 
+        }
+        return answer;
+    }
+}
