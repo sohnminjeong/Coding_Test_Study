2ae6983dc48ec38586459f59ad58ee3853905a42
Hand_min <152463277+sohnminjeong@users.noreply.github.com>
2024-07-01 03:06:28 +0900

    [level 0] Title: 각도기, Time: 0.03 ms, Memory: 83 MB -BaekjoonHub

---
+++ 프로그래머스/0/120829. 각도기/각도기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(int angle) {
+        
+        return angle==180 ? 
+            4 : (90<angle && angle<180 ? 
+                 3 : ( angle==90 ? 
+                      2 : ( 0<angle && angle < 90 ? 1 : -1 ) ) );
+    }
+}
