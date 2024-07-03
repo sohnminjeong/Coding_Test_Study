f741dc4ca659c513d5908050283f8745bacee2b7
Hand_min <152463277+sohnminjeong@users.noreply.github.com>
2024-07-04 01:25:32 +0900

    [level 0] Title: 주사위의 개수, Time: 0.03 ms, Memory: 76.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120845. 주사위의 개수/주사위의 개수.java
@@ -0,0 +1,7 @@
+class Solution {
+    public int solution(int[] box, int n) {
+        if(box[0]<n || box[1]<n || box[2]<n){return 0;}
+        else { return (box[0]/n)*(box[1]/n)*(box[2]/n);}
+        
+    }
+}
