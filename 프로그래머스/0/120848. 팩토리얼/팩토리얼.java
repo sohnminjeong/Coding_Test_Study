ceda57a9adfe4dd60972ff6c88063a71276567d4
Hand_min <152463277+sohnminjeong@users.noreply.github.com>
2024-07-04 15:28:09 +0900

    [level 0] Title: 팩토리얼, Time: 0.02 ms, Memory: 72.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120848. 팩토리얼/팩토리얼.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        int fac = 1; 
+        
+        for(int i=1; i<=10; i++){
+            fac *= i;
+            if(fac==n){
+                answer=i;
+                break;
+            } else if(fac>n){
+                answer=(i-1);
+                break;
+            }
+        }
+        return answer;
+    }
+}
