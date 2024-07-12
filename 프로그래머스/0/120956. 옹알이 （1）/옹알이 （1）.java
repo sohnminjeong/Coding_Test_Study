c31eff5e756e768990908f8d54b664b52749dcb0
Hand_min <152463277+sohnminjeong@users.noreply.github.com>
2024-07-12 14:17:38 +0900

    [level 0] Title: 옹알이 (1), Time: 0.22 ms, Memory: 70.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(String[] babbling) {
+        int answer = 0;
+        String[] yes = {"aya", "ye", "woo", "ma"};
+        for(int i=0; i<babbling.length; i++){
+            for(int j=0; j<yes.length; j++){
+                babbling[i] = babbling[i].replace(yes[j], " ");
+            }
+            if(babbling[i].trim().length()==0){
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
